96d36d66219bf173ea9bd4d1a1a334c0b3de6ff7
lololo <johndoe@example.com>
2026-06-07 17:56:00 +0700

    Fix NP0283 missing mouth

--- src/loader.js
+++ src/loader.js
@@ -31,7 +31,7 @@ import { createHaloParticleSystem } from "./haloParticles.js";
 import { createModelFx } from "./modelFx.js";
 
 let modelsIndex = [];
-const ASSET_CACHE_VERSION = "build-1"; //You should poke this after modifying the code
+const ASSET_CACHE_VERSION = "build-2"; //You should poke this after modifying the code
 const HEAD_MESH_BASE_NAMES = new Set([
   "Face",
   "Hair",
